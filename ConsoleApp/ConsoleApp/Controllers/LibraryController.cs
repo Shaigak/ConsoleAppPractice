1491de111c7484543878fa5481f2487b97f98d68
[shaigak] <shaigak@code.edu.az>
2023-01-11 21:11:40 +0300

    New

---
+++ ConsoleApp/ConsoleApp/Controllers/LibraryController.cs
@@ -0,0 +1,60 @@
+﻿using DomainLayer.Entities;
+using ServiceLayer.Helpers;
+using ServiceLayer.Services;
+using ServiceLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Controllers
+{
+    public class LibraryController
+    {
+        private readonly ILibraryService _libraryservice;
+
+        public LibraryController()
+        {
+            _libraryservice = new LibraryService();
+        }
+
+
+        public void Create()
+        {
+            ConsoleColor.Cyan.WriteConsole("Please add library name:");
+            LibraryName: string libraryName =Console.ReadLine();
+            ConsoleColor.Red.WriteConsole("Please add library seat count");
+             SeatCount: string seatCountStr=Console.ReadLine();
+            int SeatCount;
+
+            bool isCorrectSeatCount=int.TryParse(seatCountStr, out SeatCount);
+
+            if (isCorrectSeatCount)
+            {
+                try
+                {
+                    Library library = new Library
+                    {
+                        Name = libraryName,
+                        SeatCount = SeatCount
+                    };
+
+                   var response = _libraryservice.Create(library);
+                    ConsoleColor.Green.WriteConsole($"{response.Id} {response.Name} {response.SeatCount}");
+                }
+                catch (Exception ex)
+                {
+                    ConsoleColor.Red.WriteConsole(ex.Message);
+                    goto LibraryName;
+                    
+                }      
+            }
+            else
+            {
+                ConsoleColor.Cyan.WriteConsole("Please add correct format seat count");
+                goto SeatCount;
+            }
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using ConsoleApp.Controllers;
+using ServiceLayer.Helpers;
+
+LibraryController libraryController=new ();
+while (true)
+{
+    GetOptions();
+   Option:string option=Console.ReadLine();
+    int selectedOption;
+
+    bool isCorrectOption=int.TryParse(option, out selectedOption);
+    if (isCorrectOption)
+    {
+        switch (selectedOption)
+        {
+            case 1:
+                libraryController.Create();
+                Console.WriteLine("Create");
+                break;
+            case 2:
+                Console.WriteLine("Create");
+                break;
+            case 3:
+                Console.WriteLine("Create");
+                break;
+            default:
+                ConsoleColor.Red.WriteConsole("Please add correct option");
+                goto Option;          
+        }
+    }
+    else
+    {
+        ConsoleColor.Red.WriteConsole("Please add correct format option");
+        goto Option;
+    }
+}
+
+static void GetOptions()
+{
+    ConsoleColor.Cyan.WriteConsole("Please select one option");
+    ConsoleColor.Cyan.WriteConsole("Library options: 1-Create, 2-Get all, 3-Delete");
+}
